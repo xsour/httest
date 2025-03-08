ba51562994ac2d3b5a9edefbc552b0919cf77b55
stugnait <you@example.com>
2025-03-08 23:09:24 +0200

    connect to db

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', '123334')
     SESSION_TYPE = 'filesystem'
-    MYSQL_HOST = 'localhost'
+
+    MYSQL_HOST = 'yamanote.proxy.rlwy.net'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '1234'
+    MYSQL_PASSWORD = 'FOXLydlLlxTUXNwEccYjTReLkPkVJFMj'
     MYSQL_DB = 'starcourtmall'
+    MYSQL_PORT = 43735
